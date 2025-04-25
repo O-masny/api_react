93d93a184f5586cbae383ff0bde55f295ffa8830
MASNY <icelyy@seznam.cz>
2025-04-25 08:24:07 +0200

    before fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,32 @@
-import Button from "../components/button";
+import { useState, useEffect } from "react";
 
 export default function About(){
+    //definice stavu
+    const [facts, setFacts] = useState([])
+    //provedeni a zmena stavu
+    useEffect(() => {
+        //api, url 
+        let apiURL = "https://meowfacts.herokuapp.com/"
+        console.log(apiURL)
+        //api call - then převod na json - then vypsání do konzole
+        fetch(apiURL)
+        .then(response => response.json())
+        .then(data =>  {
+            console.log(data)
+            setFacts(data)
+        })
+    
+      return () => {
+      }
+    }, [])
+    
     return(
     <>
-    <div>qqweqw</div>
+    <div>
+        <img src="https://http.cat/200.jpg"></img>
+    <p>
+   </p>
+   {facts.map(fact => <div>{JSON.stringy(fact.name)}</div>)} 
+    </div>
     </>)
 }
